fce3e0b7e68802638465df54dd42b4a0ea5fb69e
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2026-02-17 14:14:11 +0800

    less aggressive painting renderer mixin

--- src/main/java/me/kall/dragit/mixin/PaintingRendererMixin.java
+++ src/main/java/me/kall/dragit/mixin/PaintingRendererMixin.java
@@ -1,67 +1,57 @@
 package me.kall.dragit.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
-import com.mojang.math.Axis;
-import it.unimi.dsi.fastutil.longs.Long2ObjectMap;
+import it.unimi.dsi.fastutil.longs.Long2ObjectMaps;
 import me.kall.dragit.data.painting.ClientPaintings;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.LevelRenderer;
-import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.PaintingRenderer;
-import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.decoration.Painting;
-import net.minecraft.world.entity.decoration.PaintingVariant;
 import org.jetbrains.annotations.NotNull;
 import org.joml.Matrix3f;
 import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PaintingRenderer.class)
 public abstract class PaintingRendererMixin extends EntityRenderer<Painting> {
-    @Shadow protected abstract void vertex(Matrix4f pose, Matrix3f normal, VertexConsumer consumer, float x, float y, float u, float v, float z, int normalX, int normalY, int normalZ, int lightmapUV);
+    @Unique private final ThreadLocal<Boolean> dropIt$usingClientPainting = new ThreadLocal<>();
 
     protected PaintingRendererMixin(EntityRendererProvider.Context context) {
         super(context);
     }
 
-    @Inject(method = "render(Lnet/minecraft/world/entity/decoration/Painting;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V", at = @At("HEAD"), cancellable = true)
-    private void renderDropped(@NotNull Painting painting, float yaw, float partialTicks, PoseStack poseStack, MultiBufferSource buffer, int packedLight, CallbackInfo ci) {
-        ResourceLocation dimension = painting.level().dimension().location();
-        long pos = painting.blockPosition().asLong();
-        Long2ObjectMap<ClientPaintings.Painting> imageMap = ClientPaintings.PAINTINGS.get(dimension);
-        if (imageMap == null) return;
-        ClientPaintings.Painting image = imageMap.get(pos);
-        if (image == null) return;
-        ci.cancel();
+    @Shadow protected abstract void vertex(Matrix4f pose, Matrix3f normal, VertexConsumer consumer, float x, float y, float u, float v, float z, int normalX, int normalY, int normalZ, int lightmapUV);
 
-        poseStack.pushPose();
-        poseStack.mulPose(Axis.YP.rotationDegrees(180.0F - yaw));
-        poseStack.scale(0.0625F, 0.0625F, 0.0625F);
+    @Inject(method = "renderPainting", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/texture/TextureAtlasSprite;getU0()F", ordinal = 0))
+    private void renderDropped(PoseStack poseStack, VertexConsumer consumer, @NotNull Painting entity, int width, int height, TextureAtlasSprite paintingSprite, TextureAtlasSprite backSprite, CallbackInfo ci, @Local PoseStack.Pose pose, @Local Matrix4f matrix4f, @Local Matrix3f matrix3f) {
+        ClientPaintings.Painting painting = ClientPaintings.PAINTINGS.getOrDefault(entity.level().dimension().location(), Long2ObjectMaps.emptyMap()).get(entity.blockPosition().asLong());
+        this.dropIt$usingClientPainting.set(painting != null);
+        this.dropIt$render(width, height, painting, entity, matrix4f, matrix3f);
+    }
 
-        PaintingVariant paintingVariant = painting.getVariant().value();
-        int width = paintingVariant.getWidth();
-        int height = paintingVariant.getHeight();
-        PoseStack.Pose pose = poseStack.last();
-        Matrix4f matrix = pose.pose();
-        Matrix3f normal = pose.normal();
-
+    @Unique
+    private void dropIt$render(int width, int height, ClientPaintings.Painting painting, @NotNull Painting entity, Matrix4f matrix4f, Matrix3f matrix3f) {
+        if (painting == null) return;
         float minX = -width / 2f;
         float minY = -height / 2f;
         int tilesX = width / 16;
         int tilesY = height / 16;
 
-        VertexConsumer imageConsumer = buffer.getBuffer(RenderType.entitySolid(image.textureLocation()));
-        int light = LevelRenderer.getLightColor(painting.level(), painting.blockPosition());
+        VertexConsumer imageConsumer = Minecraft.getInstance().renderBuffers().bufferSource().getBuffer(RenderType.entitySolid(painting.textureLocation()));
+        int light = LevelRenderer.getLightColor(entity.level(), entity.blockPosition());
 
         for (int xTile = 0; xTile < tilesX; xTile++) {
             for (int yTile = 0; yTile < tilesY; yTile++) {
@@ -75,65 +65,40 @@ public abstract class PaintingRendererMixin extends EntityRenderer<Painting> {
                 float v0 = 1.0f - (float)yTile / tilesY;
                 float v1 = 1.0f - (float)(yTile + 1) / tilesY;
 
-                imageConsumer.vertex(matrix, x1, y0, -0.5F).color(255,255,255,255).uv(u1, v0).overlayCoords(OverlayTexture.NO_OVERLAY).uv2(light).normal(normal, 0, 0, -1).endVertex();
-                imageConsumer.vertex(matrix, x0, y0, -0.5F).color(255,255,255,255).uv(u0, v0).overlayCoords(OverlayTexture.NO_OVERLAY).uv2(light).normal(normal, 0, 0, -1).endVertex();
-                imageConsumer.vertex(matrix, x0, y1, -0.5F).color(255,255,255,255).uv(u0, v1).overlayCoords(OverlayTexture.NO_OVERLAY).uv2(light).normal(normal, 0, 0, -1).endVertex();
-                imageConsumer.vertex(matrix, x1, y1, -0.5F).color(255,255,255,255).uv(u1, v1).overlayCoords(OverlayTexture.NO_OVERLAY).uv2(light).normal(normal, 0, 0, -1).endVertex();
+                this.vertex(matrix4f, matrix3f, imageConsumer, x1, y0, u1, v0, -0.5F, 0, 0, -1, light);
+                this.vertex(matrix4f, matrix3f, imageConsumer, x0, y0, u0, v0, -0.5F, 0, 0, -1, light);
+                this.vertex(matrix4f, matrix3f, imageConsumer, x0, y1, u0, v1, -0.5F, 0, 0, -1, light);
+                this.vertex(matrix4f, matrix3f, imageConsumer, x1, y1, u1, v1, -0.5F, 0, 0, -1, light);
             }
         }
+    }
 
-        TextureAtlasSprite backSprite = Minecraft.getInstance().getPaintingTextures().getBackSprite();
+    @WrapOperation(method = "renderPainting", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/entity/PaintingRenderer;vertex(Lorg/joml/Matrix4f;Lorg/joml/Matrix3f;Lcom/mojang/blaze3d/vertex/VertexConsumer;FFFFFIIII)V", ordinal = 0))
+    private void skipOrigin1(PaintingRenderer instance, Matrix4f pose, Matrix3f normal, VertexConsumer consumer, float x, float y, float u, float v, float z, int normalX, int normalY, int normalZ, int lightmapUV, Operation<Void> original) {
+        if (this.dropIt$usingClientPainting.get()) return;
+        original.call(instance, pose, normal, consumer, x, y, u, v, z, normalX, normalY, normalZ, lightmapUV);
+    }
 
-        VertexConsumer frameConsumer = buffer.getBuffer(RenderType.entitySolid(backSprite.atlasLocation()));
+    @WrapOperation(method = "renderPainting", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/entity/PaintingRenderer;vertex(Lorg/joml/Matrix4f;Lorg/joml/Matrix3f;Lcom/mojang/blaze3d/vertex/VertexConsumer;FFFFFIIII)V", ordinal = 1))
+    private void skipOrigin2(PaintingRenderer instance, Matrix4f pose, Matrix3f normal, VertexConsumer consumer, float x, float y, float u, float v, float z, int normalX, int normalY, int normalZ, int lightmapUV, Operation<Void> original) {
+        if (this.dropIt$usingClientPainting.get()) return;
+        original.call(instance, pose, normal, consumer, x, y, u, v, z, normalX, normalY, normalZ, lightmapUV);
+    }
 
-        float i = backSprite.getU0();
-        float j = backSprite.getU1();
-        float k = backSprite.getV0();
-        float l = backSprite.getV1();
-        float m = backSprite.getU0();
-        float n = backSprite.getU1();
-        float o = backSprite.getV0();
-        float p = backSprite.getV(1.0F);
-        float q = backSprite.getU0();
-        float r = backSprite.getU(1.0F);
-        float s = backSprite.getV0();
-        float t = backSprite.getV1();
+    @WrapOperation(method = "renderPainting", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/entity/PaintingRenderer;vertex(Lorg/joml/Matrix4f;Lorg/joml/Matrix3f;Lcom/mojang/blaze3d/vertex/VertexConsumer;FFFFFIIII)V", ordinal = 2))
+    private void skipOrigin3(PaintingRenderer instance, Matrix4f pose, Matrix3f normal, VertexConsumer consumer, float x, float y, float u, float v, float z, int normalX, int normalY, int normalZ, int lightmapUV, Operation<Void> original) {
+        if (this.dropIt$usingClientPainting.get()) return;
+        original.call(instance, pose, normal, consumer, x, y, u, v, z, normalX, normalY, normalZ, lightmapUV);
+    }
 
-        for (int xTile = 0; xTile < tilesX; xTile++) {
-            for (int yTile = 0; yTile < tilesY; yTile++) {
-                float x0 = minX + xTile * 16;
-                float x1 = x0 + 16;
-                float y0 = minY + yTile * 16;
-                float y1 = y0 + 16;
+    @WrapOperation(method = "renderPainting", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/entity/PaintingRenderer;vertex(Lorg/joml/Matrix4f;Lorg/joml/Matrix3f;Lcom/mojang/blaze3d/vertex/VertexConsumer;FFFFFIIII)V", ordinal = 3))
+    private void skipOrigin4(PaintingRenderer instance, Matrix4f pose, Matrix3f normal, VertexConsumer consumer, float x, float y, float u, float v, float z, int normalX, int normalY, int normalZ, int lightmapUV, Operation<Void> original) {
+        if (this.dropIt$usingClientPainting.get()) return;
+        original.call(instance, pose, normal, consumer, x, y, u, v, z, normalX, normalY, normalZ, lightmapUV);
+    }
 
-                this.vertex(matrix, normal, frameConsumer, x1, y1, j, k, 0.5F, 0, 0, 1, light);
-                this.vertex(matrix, normal, frameConsumer, x0, y1, i, k, 0.5F, 0, 0, 1, light);
-                this.vertex(matrix, normal, frameConsumer, x0, y0, i, l, 0.5F, 0, 0, 1, light);
-                this.vertex(matrix, normal, frameConsumer, x1, y0, j, l, 0.5F, 0, 0, 1, light);
-
-                this.vertex(matrix, normal, frameConsumer, x1, y1, m, o, -0.5F, 0, 1, 0, light);
-                this.vertex(matrix, normal, frameConsumer, x0, y1, n, o, -0.5F, 0, 1, 0, light);
-                this.vertex(matrix, normal, frameConsumer, x0, y1, n, p, 0.5F, 0, 1, 0, light);
-                this.vertex(matrix, normal, frameConsumer, x1, y1, m, p, 0.5F, 0, 1, 0, light);
-
-                this.vertex(matrix, normal, frameConsumer, x1, y0, m, o, 0.5F, 0, -1, 0, light);
-                this.vertex(matrix, normal, frameConsumer, x0, y0, n, o, 0.5F, 0, -1, 0, light);
-                this.vertex(matrix, normal, frameConsumer, x0, y0, n, p, -0.5F, 0, -1, 0, light);
-                this.vertex(matrix, normal, frameConsumer, x1, y0, m, p, -0.5F, 0, -1, 0, light);
-
-                this.vertex(matrix, normal, frameConsumer, x1, y1, r, s, 0.5F, -1, 0, 0, light);
-                this.vertex(matrix, normal, frameConsumer, x1, y0, r, t, 0.5F, -1, 0, 0, light);
-                this.vertex(matrix, normal, frameConsumer, x1, y0, q, t, -0.5F, -1, 0, 0, light);
-                this.vertex(matrix, normal, frameConsumer, x1, y1, q, s, -0.5F, -1, 0, 0, light);
-
-                this.vertex(matrix, normal, frameConsumer, x0, y1, r, s, -0.5F, 1, 0, 0, light);
-                this.vertex(matrix, normal, frameConsumer, x0, y0, r, t, -0.5F, 1, 0, 0, light);
-                this.vertex(matrix, normal, frameConsumer, x0, y0, q, t, 0.5F, 1, 0, 0, light);
-                this.vertex(matrix, normal, frameConsumer, x0, y1, q, s, 0.5F, 1, 0, 0, light);
-            }
-        }
-
-        poseStack.popPose();
-        super.render(painting, yaw, partialTicks, poseStack, buffer, packedLight);
+    @Inject(method = "renderPainting", at = @At("RETURN"))
+    private void renderDropped(CallbackInfo ci) {
+        this.dropIt$usingClientPainting.remove();
     }
 }
